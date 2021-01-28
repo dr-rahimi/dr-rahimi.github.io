693146baf4db5cfa5e6d5ff01359df994e18345d
Ali <dr.rahimi@outlook.com>
2021-01-28 05:25:16 +0430

    First Upload

---
+++ precache-manifest.6c7abe05e5529cfbdce601530c9c5956.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e110edb6c6712df91a0b0a66ae5a8e2",
+    "url": "/case_study.png"
+  },
+  {
+    "revision": "ef96b24d7b7dbf3cbf73",
+    "url": "/css/app.fad028a4.css"
+  },
+  {
+    "revision": "366e3d64dfad1deb10d7",
+    "url": "/css/chunk-vendors.769221c4.css"
+  },
+  {
+    "revision": "b7ae23785ec8d8e59c2b298a142c9d5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d58c27ff28281b8f5c7f",
+    "url": "/js/about.84b88912.js"
+  },
+  {
+    "revision": "ef96b24d7b7dbf3cbf73",
+    "url": "/js/app.58dfe9ec.js"
+  },
+  {
+    "revision": "366e3d64dfad1deb10d7",
+    "url": "/js/chunk-vendors.18a2c54a.js"
+  },
+  {
+    "revision": "898b020893a0521e423be84318cf1ae3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
